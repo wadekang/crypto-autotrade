fcef94ef0c337661f02f920d6d510dc7b386c304
Wade Kang <49421226+wadekang@users.noreply.github.com>
2022-01-31 18:10:42 +0900

    Update detecting_proliferation.py

--- detecting_proliferation.py
+++ detecting_proliferation.py
@@ -4,6 +4,7 @@ import requests
 import json
 import time
 import asyncio
+from sqlalchemy import true
 import websockets
 import datetime
 import logging
@@ -29,7 +30,7 @@ def get_tickers():
     remove_list = ['KRW-SNT', 'KRW-XEM', 'KRW-XLM', 'KRW-ARDR', 'KRW-TRX', 'KRW-SC', 'KRW-ZIL', 'KRW-LOOM', 'KRW-IOST', 
     'KRW-RFR', 'KRW-IQ', 'KRW-MFT', 'KRW-UPP', 'KRW-QKC', 'KRW-MOC', 'KRW-TFUEL', 'KRW-ANKR', 'KRW-AERGO', 'KRW-TT', 'KRW-CRE', 
     'KRW-MBL', 'KRW-HBAR', 'KRW-MED', 'KRW-STPT', 'KRW-ORBS', 'KRW-VET', 'KRW-CHZ', 'KRW-STMX', 'KRW-DKA', 'KRW-AHT', 'KRW-JST', 
-    'KRW-MVL', 'KRW-SSX', 'KRW-META', 'KRW-FCT2', 'KRW-HUM']
+    'KRW-MVL', 'KRW-SSX', 'KRW-META', 'KRW-FCT2', 'KRW-HUM', 'KRW-BTT', 'KRW-XEC']
     return [data['market'] for data in tickers if data['market'].startswith('KRW') and data['market'] not in remove_list]
 
 async def get_current_price(tickers):
@@ -76,8 +77,12 @@ async def main():
     tick_for_time = 'KRW-XRP'
     close_prices = None
     current_prices = None
-    buy_crpyto = None
-    target = 1.015
+    buy_crpyto = False
+    target = 1.0115
+    end_target = 1.017
+    buy_list = []
+
+    TARGET_NUM = 10
 
     while True:
         try:
@@ -91,32 +96,49 @@ async def main():
                     time.sleep(1)
                     continue
 
-                if buy_crpyto is None:
+                if not buy_crpyto:
                     # log.info('detecting...')
                     current_prices = await get_current_price(tickers)
                     for ticker in tickers:
-                        if current_prices[ticker] >= close_prices[ticker] * target:
+                        if ticker in buy_list:
+                            continue
+
+                        if close_prices[ticker] * target <= current_prices[ticker] < close_prices[ticker] * end_target:
                             log.info(f"{ticker}: detected, open={close_prices[ticker]}, target={close_prices[ticker] * target}, cur={current_prices[ticker]}")
-                            krw = upbit.get_balance("KRW")
+                            krw = upbit.get_balance("KRW") / (TARGET_NUM - len(buy_list))
                             if krw > 7000:
                                 result = upbit.buy_market_order(ticker, krw*0.9995)
                                 log.info(f"buy: {result}")
-                                buy_crpyto = ticker
-                                break
+                                buy_list.append(ticker)
+                                if len(buy_list) >= TARGET_NUM:
+                                    buy_crpyto = True
+                                    break
+                if buy_list:
+                    current_prices = await get_current_price(buy_list)
+                    for ticker in buy_list:
+                        if current_prices[ticker] >= close_prices[ticker] * end_target:
+                            crypto_balance = upbit.get_balance(ticker)
+
+                            if crypto_balance > 0:
+                                result = upbit.sell_market_order(ticker, crypto_balance)
+                                log.info(f"sell: {result}")
 
             else: # last 5 seconds -> sell and update close_prices
-                if not buy_crpyto is None:
-                    crypto_balance = upbit.get_balance(buy_crpyto)
-                    result = upbit.sell_market_order(buy_crpyto, crypto_balance*0.9995)
-                    log.info(f"sell: {result}")
-                    buy_crpyto = None
+                if buy_list:
+                    for ticker in buy_list:
+                        crypto_balance = upbit.get_balance(ticker)
 
+                        if crypto_balance > 0:
+                            result = upbit.sell_market_order(ticker, crypto_balance)
+                            log.info(f"sell: {result}")
+                    buy_list = []
+                    buy_crpyto = False
                 close_prices = await get_current_price(tickers)
                 # log.info('close_prices modify')
-            time.sleep(0.7)
+            time.sleep(0.5)
         except Exception as e:
             log.error('exception', e)
-            time.sleep(0.7)
+            time.sleep(0.5)
 
 if __name__ == "__main__":
     asyncio.run(main())
