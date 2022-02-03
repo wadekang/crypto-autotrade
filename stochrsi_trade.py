97b5a2dea3ba29095aa42d20e818e18de7ba31c4
Wade Kang <49421226+wadekang@users.noreply.github.com>
2022-02-03 23:04:12 +0900

    Create stochrsi_trade.py

---
+++ stochrsi_trade.py
@@ -0,0 +1,133 @@
+import pyupbit
+import logging
+import websockets
+import asyncio
+import json
+import sys
+import time
+import datetime
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+file_handler = logging.FileHandler('stochrsi.log')
+file_handler.setFormatter(formatter)
+log.addHandler(file_handler)
+
+access = "apikey"
+secret = "apikey"
+
+TARGET_CRYP = 'KRW-SAND'
+INTERVAL = 'minute15'
+
+TARGET_RATE = 1.011
+LIMIT = 0.993
+
+PERIOD = 14
+SMOOTH = 3
+
+async def get_current_price(ticker):
+    uri = 'wss://api.upbit.com/websocket/v1'
+    ret = None
+    async with websockets.connect(uri) as websocket:
+        subscribe_fmt = [
+                {'ticket': 'test'},
+                {
+                    'type': 'ticker',
+                    'codes': [ticker],
+                    'isOnlySnapshot': True,
+                },
+                {'format': 'SIMPLE'}
+            ]
+            
+        subscribe_data = json.dumps(subscribe_fmt)
+        await websocket.send(subscribe_data)
+
+        data = await websocket.recv()
+        data = json.loads(data)
+        ret = data['tp']
+    
+    return ret
+
+def get_start_time(ticker):
+    df = pyupbit.get_ohlcv(ticker, interval=INTERVAL, count=1)
+    return df.index[0]
+
+def stochastic_rsi(ticker):
+    df = pyupbit.get_ohlcv(ticker, INTERVAL, count=35)
+    delta = df['close'].diff(1).dropna()
+    up = delta.copy()
+    down = delta.copy()
+    up[up < 0] = 0
+    down[down > 0] = 0
+
+    AVG_GAIN = up.ewm(com=PERIOD-1, min_periods=PERIOD).mean()
+    AVG_LOSS = abs(down.ewm(com=PERIOD-1, min_periods=PERIOD).mean())
+    RS = AVG_GAIN / AVG_LOSS
+    RSI = 100.0 - (100.0 / (1.0 + RS))
+    df['rsi'] = RSI
+
+    min_val = df['rsi'].rolling(window=PERIOD, center=False).min()
+    max_val = df['rsi'].rolling(window=PERIOD, center=False).max()
+    stoch = ((df['rsi'] - min_val) / (max_val - min_val)) * 100
+    K = round(stoch.rolling(window=SMOOTH, center=False).mean(), 2)
+    # D = round(K.rolling(window=SMOOTH, center=False).mean(), 2)
+
+    return K[-4:-1]
+
+async def main():
+    upbit = pyupbit.Upbit(access, secret)
+    balance = upbit.get_balance("KRW")
+    if balance == None:
+        log.error('api key error')
+        sys.exit('api key error')
+
+    log.info('autotrade start')
+
+    buy_cryp = False
+    buy_price = None
+    stoch_rsi = None
+
+    while True:
+        try:
+            stoch_rsi = stochastic_rsi(TARGET_CRYP)
+            cur = stoch_rsi[-1]
+            before = stoch_rsi[-2]
+
+            if buy_cryp:
+                if cur <= before - 3:
+                    cryp = upbit.get_balance(TARGET_CRYP)
+
+                    if cryp > 0:
+                        result = upbit.sell_market_order(TARGET_CRYP, cryp)
+                        log.info(f"sell: {result}")
+                        buy_cryp = False
+
+                else:
+                    current_price = await get_current_price(TARGET_CRYP)
+
+                    if current_price < buy_price * LIMIT: # stop loss
+                        cryp = upbit.get_balance(TARGET_CRYP)
+
+                        if cryp > 0:
+                            result = upbit.sell_market_order(TARGET_CRYP, cryp)
+                            log.info(f"stop loss: {result}")
+                            buy_cryp = False
+            else:
+                if cur >= before + 3:
+                    krw = upbit.get_balance('KRW')
+
+                    if krw > 21000:
+                        buy_price = await get_current_price(TARGET_CRYP)
+                        result = upbit.buy_market_order(TARGET_CRYP, 20000)
+                        log.info(f"buy: {result}")
+                        buy_cryp = True
+
+            time.sleep(1)
+        except Exception as e:
+            log.error(e)
+            time.sleep(1)
+
+if __name__ == "__main__":
+    asyncio.run(main())
